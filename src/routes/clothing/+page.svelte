b2100aff7ebf44419f96685fffb75ce9c69d65f1
Rory Xavier <84554571+roryxavier@users.noreply.github.com>
2023-03-31 00:04:42 +0800

    Reference Page (fet): Add Reference (fet): Add Colour Palette (fix): Change Home Page (fix): Fix metadata of icon (fet): Add initial page layout for clothing

--- src/routes/clothing/+page.svelte
+++ src/routes/clothing/+page.svelte
@@ -1,7 +1,31 @@
 <script>
 	import UnderConstruction from '../../comps/UnderConstruction.svelte';
+	import ICON_ARROW from '../../assets/arrow_224071.svg';
+	import SectionTitle from '../../comps/common/SectionTitle.svelte';
+	import ResponsiveContainer from '../../comps/common/ResponsiveContainer.svelte';
+	import ImageCard from './ImageCard.svelte';
+	import GalleryNavigator from './GalleryNavigator.svelte';
 </script>
 
 <main>
 	<UnderConstruction />
+	<!--	<ResponsiveContainer>-->
+	<!--		<div class='flex flex-col content-center items-center'>-->
+	<!--		<SectionTitle title='Clothing Styles' id_tag='cloth_style' />-->
+	<!--			<div class='flex flex-row space-x-3 items-center'>-->
+	<!--				<div class='w-7 m-0'>-->
+	<!--					<button><img class='rotate-90' src={ICON_ARROW} alt='Arrow Left' /></button>-->
+	<!--				</div>-->
+	<!--				<div class='w-[100vh] '>-->
+	<!--					<ImageCard></ImageCard>-->
+	<!--				</div>-->
+	<!--				<div>-->
+	<!--					<button><img class='-rotate-90 w-7' src={ICON_ARROW} alt='Arrow Right' /></button>-->
+	<!--				</div>-->
+
+	<!--			</div>-->
+	<!--			<div class='h-5'></div>-->
+	<!--			<GalleryNavigator counter={10} current_index={2}/>-->
+	<!--		</div>-->
+	<!--	</ResponsiveContainer>-->
 </main>
---
+++ src/routes/reference/TableColour.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	const body_parts = [
+		'Main Body Part',
+		'Hair Soft Colour',
+		'Hair Secondary Colour',
+		'Inner Body Part',
+		'Inner Body Part Line',
+		'Leg',
+		'Tail First layer',
+		'Tail Second Layer',
+		'Eye outer colour',
+		'Eye Half Top Ring',
+		'Eye Half Bottom Ring',
+		'Eye Middle'
+	];
+	const colour_value = [
+		'#6bc5fa',
+		'#44c8ff',
+		'#5a9be9',
+		'#ffffff',
+		'#0f0f0f',
+		'#656469',
+		'#00019a',
+		'#10124b',
+		'#fff4de',
+		'#5d9aeb',
+		'#7af9fe',
+		'#00024c'
+	];
+</script>
+
+<table class="w-full table-fixed" style="border-bottom: 1px solid indigo">
+	<div class="Row Row-title">
+		<th class="Row-key">Body Parts</th>
+		<th class="Row-key">Value</th>
+		<th class="Row-key">Colour</th>
+	</div>
+	{#each Array(body_parts.length) as _, i}
+		<tr class="Row Row-content">
+			<td class="Row-key">{body_parts[i]}</td>
+			<td class="Row-key">{colour_value[i].toUpperCase()}</td>
+			<td class="Row-key" style="padding: 0"
+				><div class="h-full p-0" style="background-color: {colour_value[i]};" /></td
+			>
+		</tr>
+	{/each}
+</table>
+
+<style lang="scss">
+	.Row {
+		display: flex;
+		flex-direction: row;
+		justify-content: stretch;
+		gap: 2rem;
+
+		& > * {
+			width: 100%;
+			flex-grow: 1;
+			padding: 0.3rem;
+		}
+
+		.Row-key {
+			text-align: end;
+		}
+
+		.Row-value {
+			text-align: start;
+		}
+	}
+
+	.Row-title {
+		font-weight: 800;
+		border: 1px solid indigo;
+		border-left: none;
+		border-right: none;
+	}
+
+	.Row-content {
+		transition: 200ms;
+
+		&:hover {
+			background-color: rgba(0, 0, 0, 0.2);
+		}
+	}
+</style>
