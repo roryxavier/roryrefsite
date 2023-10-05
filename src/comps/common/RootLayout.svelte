7eac7f76c63522404fa0d3c211b54ea982483d72
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-10-05 22:47:58 +0800

    testing transition

--- src/comps/common/RootLayout.svelte
+++ src/comps/common/RootLayout.svelte
@@ -1,23 +1,47 @@
-<script>
+<script lang="ts">
 	import NavBar from '../header/NavBar.svelte';
 	import Footer from '../footer/Footer.svelte';
+	import { onMount } from 'svelte';
+	import { navigating } from '$app/stores';
 
-	let navbar;
-	let app;
+	$: if ($navigating) onRouteChange();
+
+	let navbar: typeof NavBar;
+	let app: HTMLDivElement;
+
+	let backgroundShow = false;
+	let appShow = false;
+	let containerShow = false;
+
+	const onRouteChange = () => {
+		containerShow = false;
+
+		setTimeout(() => {
+			containerShow = true;
+		}, 200);
+	};
+
+	onMount(() => {
+		backgroundShow = true;
+		appShow = true;
+		containerShow = true;
+	});
 </script>
 
 <svelte:head>
 	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet" />
 </svelte:head>
 
+<div class="background background-{backgroundShow}" />
+
 <div
-	class="AppPage"
+	class="AppPage AppPage-{appShow}"
 	bind:this={app}
 	on:scroll={(e) => (navbar.isScrollDown = e.target.scrollTop > 4)}
 >
 	<NavBar bind:this={navbar} onItemSelected={() => (app.scrollTop = 0)} />
 
-	<div class="AppPage-container">
+	<div class="AppPage-container AppPage-container-{containerShow}">
 		<slot />
 	</div>
 
@@ -25,6 +49,26 @@
 </div>
 
 <style lang="scss">
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+	.background {
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: calc(100dvh + 10rem);
+		width: 100vw;
+		background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+
+		transition: all 1000ms ease;
+		opacity: 0;
+	}
+	.background-true {
+		opacity: 1;
+	}
+
 	:root {
 		@media (max-width: 700px) {
 			font-size: 19px;
@@ -72,9 +116,20 @@
 		height: max-content;
 		width: 100%;
 		position: relative;
+		transition: all 400ms ease;
+		opacity: 0;
+
+		.AppPage-container {
+			z-index: 1;
+			flex-grow: 1;
+			transition: all 200ms ease;
+			transform: translateY(-0.5rem);
+		}
+		.AppPage-container-true {
+			transform: translateY(0);
+		}
 	}
-	.AppPage-container {
-		z-index: 1;
-		flex-grow: 1;
+	.AppPage-true {
+		opacity: 1;
 	}
 </style>
